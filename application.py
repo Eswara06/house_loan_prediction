cc5844101695a9038ff41aef3ad72bcda0d57af7
Eswara Sai Sumanth Mamidi <emamidi@Eswaras-MacBook-Pro-2.local>
2024-11-24 16:41:26 -0500

    update html&app

--- application.py
+++ application.py
@@ -14,11 +14,40 @@ def hello_world():
     return render_template('index.html')
 
 @app.route('/predictdata', methods=['GET', 'POST'])
-def predict_datapoint():
-    if request.method =='POST':
-        pass
+def predict_data():
+    if request.method == 'POST':
+       
+        data = request.form
+
+        
+        features = [
+            data.get('Gender'),
+            data.get('Married'),
+            data.get('Dependents'),
+            data.get('Education'),
+            data.get('Self_Employed'),
+            data.get('ApplicantIncome'),
+            data.get('CoapplicantIncome'),
+            data.get('LoanAmount'),
+            data.get('Loan_Amount_Term'),
+            data.get('Credit_History'),
+            data.get('Property_Area')
+        ]
+
+        
+        input_array = np.array(features).reshape(1, -1)
+
+        
+        scaler = StandardScaler()
+        input_array_scaled = scaler.fit_transform(input_array)
+        prediction = model.predict(input_array_scaled)
+        result = "Approved" if prediction[0] == 1 else "Rejected"
+        return render_template('home.html', result=result)
+
     else:
-        return render_template('home.html')
+        
+        return render_template('home.html', result=None)
+
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0')
+    app.run(port=5001)
